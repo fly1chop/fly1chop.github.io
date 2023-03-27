c708f74f9335aed6e1153aebd4e97ebbc41df16d
Jane Ro <janero95@gmail.com>
2023-03-28 00:30:59 +0900

    feat: getStaticProps from notion database

---
+++ src/types/post.d.ts
@@ -0,0 +1,38 @@
+import {
+  QueryDatabaseResponse,
+  PageObjectResponse
+} from '@notionhq/client/build/src/api-endpoints';
+
+export interface Tag {
+  id: string;
+  name: string;
+  color: string;
+}
+
+export interface PostResponse {
+  id: string;
+  title: string;
+  tags: Tag[];
+  date: string;
+}
+
+export type PostResult = Extract<
+  QueryDatabaseResponse['results'][number],
+  { properties: Record<string, unknown> }
+>;
+
+type PropertyValueMap = PostResult['properties'];
+type PropertyValue = PropertyValueMap[string];
+type PropertyValueType = PropertyValue['type'];
+
+export type ExtractedPropertyValue<TType extends PropertyValueType> = Extract<
+  PropertyValue,
+  { type: TType }
+>;
+
+export type DatabaseItem = PostResult & {
+  properties: {
+    Name: ExtractedPropertyValue<'title'>;
+    Tags: ExtractedPropertyValue<'multi_select'>;
+  };
+};
