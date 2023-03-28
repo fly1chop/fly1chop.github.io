5cea1aa0dcdfc0817069e28b363ce7dc7a5165b9
Jane Ro <janero95@gmail.com>
2023-03-28 22:15:58 +0900

    Updates

---
+++ _next/static/chunks/pages/portfolio-8749e078bbfb4ce9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[371],{4904:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/portfolio",function(){return t(2561)}])},2561:function(n,u,t){"use strict";t.r(u);var _=t(5893);let i=()=>(0,_.jsx)("div",{});u.default=i}},function(n){n.O(0,[774,888,179],function(){return n(n.s=4904)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/pages/post/[slug]-9acf38a72585873f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[515],{1705:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/post/[slug]",function(){return t(2791)}])},2791:function(n,u,t){"use strict";t.r(u);var _=t(5893);let s=()=>(0,_.jsx)("div",{});u.default=s}},function(n){n.O(0,[774,888,179],function(){return n(n.s=1705)}),_N_E=n.O()}]);
