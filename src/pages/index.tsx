8d63ee1871e9c2ec021a5bc1f7f6bb3548517acb
Jane Ro <janero95@gmail.com>
2023-03-29 01:38:53 +0900

    refactor: extract notion functions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { styles, PostGrid, TagList } from '@/components/Home';
-import { Client } from '@notionhq/client';
 import { InferGetStaticPropsType } from 'next';
-import { ResultItem } from '@/types/post';
+import { getAllPosts } from '@/utils/notion';
 
 const HomePage = ({
   posts
@@ -17,26 +16,7 @@ const HomePage = ({
 export default HomePage;
 
 export async function getStaticProps() {
-  const notion = new Client({
-    auth: process.env.NOTION_ACCESS_TOKEN
-  });
-  const databaseId = process.env.NOTION_DATABASE_ID as string;
-  const response = await notion.databases.query({
-    database_id: databaseId
-  });
-
-  const formatResponse = (post: ResultItem) => {
-    return {
-      id: post.id,
-      date: post.created_time,
-      title: post.properties.Name.title[0].plain_text,
-      tags: post.properties.Tags.multi_select
-    };
-  };
-
-  const posts = response.results.map(result =>
-    formatResponse(result as ResultItem)
-  );
+  const posts = await getAllPosts();
 
   return {
     props: {
