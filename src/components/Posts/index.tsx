c5e7b95fd16ca74217c9688a4c95fbb35a4c06f3
Jane Ro <janero95@gmail.com>
2023-04-12 00:38:41 +0900

    style: post page

---
+++ src/components/Posts/index.tsx
@@ -0,0 +1,3 @@
+export { default as styles } from './posts.module.scss';
+export { default as NotionContent } from './notion-content';
+export { default as TableOfContent } from './table-of-content';
