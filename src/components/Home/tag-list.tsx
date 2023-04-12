1e6b32201338b4a7f38d8c7e7143880cec26daac
Jane Ro <janero95@gmail.com>
2023-04-12 18:47:41 +0900

    feat: handle click to dynamically add classnames

--- src/components/Home/tag-list.tsx
+++ src/components/Home/tag-list.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import Tag from '../base/Tag';
-import styles from './home.module.css';
+import styles from './home.module.scss';
 
 const tags = ['Next', 'React', 'JS', 'TS', 'HTML'];
 
 const TagList = () => {
+  const [filters, setFilters] = useState<string[]>([]);
+
+  const handleSelect = (tag: string) => {
+    if (filters.includes(tag)) {
+      setFilters(filters.filter(x => x !== tag));
+    } else {
+      setFilters([...filters, tag]);
+    }
+  };
+
   return (
     <div className={styles.tagList}>
       <ul>
         {tags.map(tag => (
-          <Tag name={tag} fill={true} size="large" key={tag} />
+          <button key={tag} onClick={() => handleSelect(tag)}>
+            <Tag name={tag} fill={filters.includes(tag)} size="large" />
+          </button>
         ))}
       </ul>
     </div>
